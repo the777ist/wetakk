b5d39a5fabc709e509c704685bb497f084b58f13
Hrittik Bhattacharjee <hrittikcse@outlook.com>
2019-11-13 20:49:00 +0530

    initial commit

---
+++ wetakk-client/src/components/Homepage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import MessageTimeline from "./MessageTimeline";
+
+const Homepage = ({ currentUser }) => {
+	if(!currentUser.isAuthenticated) {
+		return(
+			<div className="home-hero">
+				<h1>Welcome to WeTakk</h1>
+				<h3>A great place to connect with family and friends!</h3>
+				<Link to="/signup" className="btn btn-primary">Sign Up</Link>
+			</div>
+		);
+	}
+	return (
+		<div>
+			<MessageTimeline profileImageUrl={currentUser.user.profileImageUrl} username={currentUser.user.username} />
+		</div>
+	);
+};
+
+export default Homepage;
